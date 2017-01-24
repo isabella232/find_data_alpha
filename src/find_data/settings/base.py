1fc4bc8288376afaca0e6088ed017476a87fab45
Ross Jones <ross@servercode.co.uk>
2017-01-24 15:47:35 +0000

    Remove DB support

--- src/find_data/settings/base.py
+++ src/find_data/settings/base.py
@@ -87,18 +87,12 @@ WSGI_APPLICATION = 'find_data.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-if os.environ.get('DATABASE_URL'):
-    import dj_database_url
-    DATABASES = {}
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
+}
 
 
 # Password validation
